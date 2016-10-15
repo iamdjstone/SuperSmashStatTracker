d6c8ba452f9dc08d98b2d2dc4819b60f520e25d6
Gunner Bills <gnbills@bsu.edu>
2016-10-15 15:19:03 -0400

    Create methods to add wins and losses, as well as methods to return those values and total matches to the StatCollector class

--- app/src/test/java/edu/bsu/billsgreenwaymalchow/supersmashstattracker/ExampleUnitTest.java
+++ app/src/test/java/edu/bsu/billsgreenwaymalchow/supersmashstattracker/ExampleUnitTest.java
@@ -10,8 +10,22 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    StatCollector stats = new StatCollector();
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void percentage_isCorrect(){
+        stats.addWin();
+        stats.addWin();
+        stats.addWin();
+        stats.addLoss();
+        stats.addLoss();
+        stats.addLoss();
+        assertEquals(0.5, stats.getWinPercentage(), 0.0);
     }
 }
