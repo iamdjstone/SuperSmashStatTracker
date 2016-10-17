d6f54795b2975b4086eb981c808ab8d02c023a9a
Dakota Malchow <dakotamalchow@gmail.com>
2016-10-17 19:22:32 -0400

    Refactor MainActivity into seperate Activities

--- app/src/main/java/edu/bsu/billsgreenwaymalchow/supersmashstattracker/MainActivity.java
+++ app/src/main/java/edu/bsu/billsgreenwaymalchow/supersmashstattracker/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.bsu.billsgreenwaymalchow.supersmashstattracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,39 +9,15 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    WinLossCounter winLossCounter = new WinLossCounter();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button startButton = (Button) findViewById(R.id.startButton);
         startButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
-                final EditText winPercentage = (EditText) findViewById(R.id.percentageEditText);
-                setContentView(R.layout.win_loss);
-                Button addWinButton = (Button) findViewById(R.id.addWinButton);
-                final EditText totalWins = (EditText) findViewById(R.id.winTotalEditText);
-                addWinButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        winLossCounter.addWin();
-                        totalWins.setText(Integer.toString(winLossCounter.getWins()));
-                        //winPercentage.setText(Double.toString(winLossCounter.getWinPercentage()));
-                    }
-                });
-                Button addLossButton = (Button) findViewById(R.id.addLossButton);
-                final EditText totalLosses = (EditText) findViewById(R.id.lossTotalEditText);
-                addLossButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        winLossCounter.addLoss();
-                        totalLosses.setText(Integer.toString(winLossCounter.getLosses()));
-                        //winPercentage.setText(Double.toString(winLossCounter.getWinPercentage()));
-                    }
-                });
+                startActivity(new Intent(MainActivity.this, WinLossActivity.class));
             }
         });
     }
--- app/src/main/java/edu/bsu/billsgreenwaymalchow/supersmashstattracker/WinLossActivity.java
+++ app/src/main/java/edu/bsu/billsgreenwaymalchow/supersmashstattracker/WinLossActivity.java
@@ -2,11 +2,37 @@ package edu.bsu.billsgreenwaymalchow.supersmashstattracker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class WinLossActivity extends AppCompatActivity{
+public class WinLossActivity extends AppCompatActivity {
 
-    protected void onCreate(Bundle savedInstanceState){
+    WinLossCounter winLossCounter = new WinLossCounter();
+
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.win_loss);
+        final EditText winPercentage = (EditText) findViewById(R.id.percentageEditText);
+        Button addWinButton = (Button) findViewById(R.id.addWinButton);
+        final EditText totalWins = (EditText) findViewById(R.id.winTotalEditText);
+        addWinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                winLossCounter.addWin();
+                totalWins.setText(Integer.toString(winLossCounter.getWins()));
+                //winPercentage.setText(Double.toString(winLossCounter.getWinPercentage()));
+            }
+        });
+        Button addLossButton = (Button) findViewById(R.id.addLossButton);
+        final EditText totalLosses = (EditText) findViewById(R.id.lossTotalEditText);
+        addLossButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                winLossCounter.addLoss();
+                totalLosses.setText(Integer.toString(winLossCounter.getLosses()));
+                //winPercentage.setText(Double.toString(winLossCounter.getWinPercentage()));
+            }
+        });
     }
 }
