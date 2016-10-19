e09bef6093a516f42916fcbef907f411cf3cf19d
Dakota Malchow <dakotamalchow@gmail.com>
2016-10-19 11:59:45 -0400

    Add create tracker activity and back buttons

---
+++ app/src/main/java/edu/bsu/billsgreenwaymalchow/supersmashstattracker/CreateStatTrackerActivity.java
@@ -0,0 +1,13 @@
+package edu.bsu.billsgreenwaymalchow.supersmashstattracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateStatTrackerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_stat_tracker);
+    }
+}
--- app/src/main/java/edu/bsu/billsgreenwaymalchow/supersmashstattracker/StatTrackerActivity.java
+++ app/src/main/java/edu/bsu/billsgreenwaymalchow/supersmashstattracker/StatTrackerActivity.java
@@ -1,7 +1,10 @@
 package edu.bsu.billsgreenwaymalchow.supersmashstattracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class StatTrackerActivity extends AppCompatActivity{
 
@@ -9,5 +12,12 @@ public class StatTrackerActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stat_tracker_list);
+        Button createStatTrackerButton = (Button) findViewById(R.id.create_stat_tracker_button);
+        createStatTrackerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StatTrackerActivity.this, CreateStatTrackerActivity.class));
+            }
+        });
     }
 }
