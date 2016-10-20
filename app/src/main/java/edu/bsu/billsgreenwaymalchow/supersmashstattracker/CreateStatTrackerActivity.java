f1aa67be377511c21f81a30914c4eda16e1cc1e1
Danny Greenway <dpgreenway@bsu.edu>
2016-10-20 17:38:35 -0400

    Create a button in the correct place in the Create Stat Tracker Activity

--- app/src/main/java/edu/bsu/billsgreenwaymalchow/supersmashstattracker/CreateStatTrackerActivity.java
+++ app/src/main/java/edu/bsu/billsgreenwaymalchow/supersmashstattracker/CreateStatTrackerActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
 public class CreateStatTrackerActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +18,15 @@ public class CreateStatTrackerActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Button submitButton = (Button) findViewById(R.id.submit_button);
+        final Button statTrackerButton = new Button(this);
         final EditText trackerName = (EditText) findViewById(R.id.trackerName);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ScrollView statTrackerScrollView = (ScrollView) findViewById(R.id.stat_tracker_scroll_view);
-                Button statTrackerButton = (Button) new View(this);
-                statTrackerScrollView.addView(statTrackerButton);
+                ScrollView.LayoutParams scrollViewLayout = new ScrollView.LayoutParams(ScrollView.LayoutParams.MATCH_PARENT, ScrollView.LayoutParams.WRAP_CONTENT);
                 statTrackerButton.setText(trackerName.getText().toString());
+                scrollViewLayout.addView()
                 finish();
             }
         });
