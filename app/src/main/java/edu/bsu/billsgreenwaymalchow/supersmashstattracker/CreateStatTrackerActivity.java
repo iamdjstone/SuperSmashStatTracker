9ffb9df23fdbaa15c92b95991ae2490181048419
Dakota Malchow <dakotamalchow@gmail.com>
2016-10-21 12:46:53 -0400

    Add Buttons to choose game version

--- app/src/main/java/edu/bsu/billsgreenwaymalchow/supersmashstattracker/CreateStatTrackerActivity.java
+++ app/src/main/java/edu/bsu/billsgreenwaymalchow/supersmashstattracker/CreateStatTrackerActivity.java
@@ -13,7 +13,9 @@ import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.Toast;
 
-public class CreateStatTrackerActivity extends AppCompatActivity {
+public class CreateStatTrackerActivity extends AppCompatActivity implements View.OnClickListener {
+
+    StatTracker statTracker = new StatTracker();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +23,57 @@ public class CreateStatTrackerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_stat_tracker);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        Button gameVersion64 = (Button) findViewById(R.id.game_version_64);
+        gameVersion64.setOnClickListener(this);
+        Button gameVersionMelee = (Button) findViewById(R.id.game_version_melee);
+        gameVersionMelee.setOnClickListener(this);
+        Button gameVersionBrawl = (Button) findViewById(R.id.game_version_brawl);
+        gameVersionBrawl.setOnClickListener(this);
+        Button gameVersionWiiU = (Button) findViewById(R.id.game_version_wii_u);
+        gameVersionWiiU.setOnClickListener(this);
+        Button gameVersion3DS = (Button) findViewById(R.id.game_version_3ds);
+        gameVersion3DS.setOnClickListener(this);
         Button submitButton = (Button) findViewById(R.id.submit_button);
-        final EditText trackerName = (EditText) findViewById(R.id.trackerName);
+        submitButton.setOnClickListener(this);
+    }
 
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    public void showToastMessage(String gameVersion){
+        String s = "You selected game version: ";
+        Toast.makeText(CreateStatTrackerActivity.this, s + gameVersion, Toast.LENGTH_SHORT).show();
+        statTracker.setGameVersion(gameVersion);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+
+            case R.id.game_version_64:
+                showToastMessage("Nintendo 64");
+                break;
+
+            case R.id.game_version_melee:
+                showToastMessage("Melee");
+                break;
+
+            case R.id.game_version_brawl:
+                showToastMessage("Brawl");
+                break;
+
+            case R.id.game_version_wii_u:
+                showToastMessage("Wii U");
+                break;
+
+            case R.id.game_version_3ds:
+                showToastMessage("3DS");
+                break;
+
+            case R.id.submit_button:
                 Intent intent = new Intent();
+                final EditText trackerName = (EditText) findViewById(R.id.trackerName);
                 intent.putExtra("trackerName", trackerName.getText().toString());
                 setResult(RESULT_OK, intent);
                 finish();
-            }
-        });
+                break;
+        }
     }
 }
---
+++ app/src/main/java/edu/bsu/billsgreenwaymalchow/supersmashstattracker/StatTracker.java
@@ -0,0 +1,15 @@
+package edu.bsu.billsgreenwaymalchow.supersmashstattracker;
+
+public class StatTracker {
+
+    private String name;
+    private String gameVersion;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGameVersion(String gameVersion){
+        this.gameVersion = gameVersion;
+    }
+}
