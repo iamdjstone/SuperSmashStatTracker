b143208f9e78e922d375fbeb57bb9269675eb41d
Danny Greenway <dpgreenway@bsu.edu>
2016-10-21 11:44:05 -0400

    Add full functionality of accessing a created stat tracker and adding wins or losses to stat tracker

--- app/src/main/java/edu/bsu/billsgreenwaymalchow/supersmashstattracker/StatTrackerActivity.java
+++ app/src/main/java/edu/bsu/billsgreenwaymalchow/supersmashstattracker/StatTrackerActivity.java
@@ -1,16 +1,16 @@
 package edu.bsu.billsgreenwaymalchow.supersmashstattracker;
 
-import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 
-public class StatTrackerActivity extends AppCompatActivity implements View.OnClickListener{
+public class StatTrackerActivity extends AppCompatActivity{
+
+    TrackerList trackerList = new TrackerList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,29 +19,33 @@ public class StatTrackerActivity extends AppCompatActivity implements View.OnCli
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Button createStatTrackerButton = (Button) findViewById(R.id.create_stat_tracker_button);
-        createStatTrackerButton.setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onClick(View v) {
-        Intent i = new Intent(this, CreateStatTrackerActivity.class);
-        startActivityForResult(i, 1);
+        LinearLayout listOfButtons = (LinearLayout) findViewById(R.id.linear_layout_scrollbar);
+        createStatTrackerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(StatTrackerActivity.this, CreateStatTrackerActivity.class);
+                startActivityForResult(i, 1);
+            }
+        });
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode,resultCode,data);
-        if (requestCode == 1){
-            if(resultCode == RESULT_OK){
+        if ((requestCode == 1) && (resultCode== RESULT_OK)){
                 String newName = data.getStringExtra("trackerName");
                 Button thisStatTrackerButton = new Button(this);
                 thisStatTrackerButton.setText(newName);
                 LinearLayout statTrackerScrollList = (LinearLayout)findViewById(R.id.linear_layout_scrollbar);
-                LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+                LayoutParams lp = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
                 statTrackerScrollList.addView(thisStatTrackerButton, lp);
-
+                trackerList.addTracker(thisStatTrackerButton);
+                thisStatTrackerButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(StatTrackerActivity.this, WinLossActivity.class));
+                }
+            });
             }
         }
     }
-}
