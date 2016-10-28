7a42e0729f9be47435c0d0c46c6c73b75dc08b8d
Gunner Bills <gnbills@bsu.edu>
2016-10-28 12:15:56 -0400

    Create database open helper.

--- app/src/main/java/edu/bsu/billsgreenwaymalchow/supersmashstattracker/MainActivity.java
+++ app/src/main/java/edu/bsu/billsgreenwaymalchow/supersmashstattracker/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.bsu.billsgreenwaymalchow.supersmashstattracker;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,5 +20,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, StatTrackerActivity.class));
             }
         });
+        DatabaseOpenHelper databaseOpenHelper = new DatabaseOpenHelper(this);
+        SQLiteDatabase sqLiteDatabase = databaseOpenHelper.getWritableDatabase();
     }
 }
